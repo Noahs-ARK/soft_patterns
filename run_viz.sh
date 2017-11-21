2c950256d004069ab4be22ecea06d93e60d5ad07
Roy Schwartz <tg845976@login4.stampede2.tacc.utexas.edu>
2017-11-21 14:21:59 -0600

    run_viz.sh accepts an output file and not a model directory

--- run_viz.sh
+++ run_viz.sh
@@ -2,32 +2,41 @@
 
 set -e
 
+model_num=-1
+
 if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <model dir> <model number (optional, otherwise select max dev value)>"
+    echo "Usage: $0 <output file> <model number (optional, otherwise select max dev value)>"
     exit -1
 elif [ "$#" -gt 1 ]; then
     model_num=$2
-else
-    i=1
-    model_num=-1
-    while [ ! -e ${1}/model_${model_num}.pth ];
-    do
-	model_num=$(grep loss $1/output.dat | awk '{print $2" "$NF}' | sort -rnk2 | sed "${i}q;d" | awk '{print $1}')
-	let i++
-    done
 fi
 
-model_dir=$1
+f=$1
 
 function get_param {
     local f=$1
     local name=$2
 
-    val=$(head -1 ${f} | tr ' ' '\n' | grep "${name}=" | cut -d '=' -f2 | tr -d "'()" | sed 's/,$//')
+    val=$(head -1 ${f} | tr ' ' '\n' | grep -E "\b${name}=" | cut -d '=' -f2 | tr -d "'()" | sed 's/,$//')
     echo ${val}
 }
 
-f="${model_dir}/output.dat"
+model_dir=$(get_param $f model_save_dir)
+
+if [ ${model_num} -eq -1 ]; then
+    i=1
+    while [ ! -e ${model_dir}/model_${model_num}.pth ];
+    do
+	model_num=$(grep loss $f | awk '{print $2" "$NF}' | sort -rnk2 | sed "${i}q;d" | awk '{print $1}')
+	let i++
+    done
+fi
+
+if [ ${model_num} -eq -1 ]; then
+	echo "Couldn't find model file in $model_dir"
+	exit -2
+fi
+
 
 e=$(get_param ${f} embedding_file)
 maxplus=$(get_param ${f} maxplus)
