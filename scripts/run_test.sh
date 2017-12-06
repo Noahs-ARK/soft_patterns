ff1f5e1100086f65e989a1563da704a131893240
Roy Schwartz <roysch@164.7.208.128.dynr.cs.washington.edu>
2017-12-05 17:32:47 -0800

    run_test.sh now supports dan and lstm

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,15 +4,16 @@ set -e
 
 model_num=-1
 
-if [ "$#" -lt 4 ]; then
-    echo "Usage: $0 <output file> <model number> <test data> <test labels>"
+if [ "$#" -lt 5 ]; then
+    echo "Usage: $0 <output file> <model: 0 for soft_patterns, 1 for dan, 2 for lstm> <model number> <test data> <test labels>"
     exit -1
 fi
 
 f=$1
-model_num=$2
-test_data=$3
-test_labels=$4
+model=$2
+model_num=$3
+test_data=$4
+test_labels=$5
 
 function get_param {
     local f=$1
@@ -41,8 +42,9 @@ fi
 
 e=$(get_param ${f} embedding_file)
 gpu=$(get_param ${f} gpu)
-maxplus=$(get_param ${f} maxplus)
 seed=$(get_param ${f} seed)
+mlp_hidden_dim=$(get_param ${f} mlp_hidden_dim)
+num_mlp_layers=$(get_param ${f} num_mlp_layers)
 
 if [ "${gpu}" == 'True' ]; then
     gpu='--gpu'
@@ -50,34 +52,43 @@ else
     gpu=''
 fi
 
-if [ "${maxplus}" == 'True' ]; then
-    maxplus="--maxplus"
-else
-    maxtimes=$(get_param ${f} maxtimes)
-    if [ "${maxtimes}" == 'True' ]; then
-        maxplus="--maxtimes"
+if [ $model -eq 0 ]; then
+    maxplus=$(get_param ${f} maxplus)
+
+    if [ "${maxplus}" == 'True' ]; then
+        maxplus="--maxplus"
     else
-        maxplus=''
+        maxtimes=$(get_param ${f} maxtimes)
+        if [ "${maxtimes}" == 'True' ]; then
+            maxplus="--maxtimes"
+        else
+            maxplus=''
+        fi
     fi
+    patterns=$(get_param ${f} patterns)
+
+    s="-p $patterns $maxplus"
+elif [ $model -eq 1 ]; then
+    s="--dan"
+elif [ $model -eq 2 ]; then
+    lstm_hidden=$(get_param ${f} hidden_dim)
+    s="--bilstm --hidden_bilstm_dim $lstm_hidden"
+else
+    echo "Model not found (should be 0, 1 or 2. Got $model"
+    exit -3
 fi
-
-
-mlp_hidden_dim=$(get_param ${f} mlp_hidden_dim)
-num_mlp_layers=$(get_param ${f} num_mlp_layers)
-patterns=$(get_param ${f} patterns)
 
 
 com="python -u soft_patterns_test.py  \
     -e ${e} \
-    -p ${patterns} \
     --vd ${test_data} \
     --vl ${test_labels} \
     --td '' \
     --tl '' \
     -d ${mlp_hidden_dim} \
-    ${maxplus} \
     --seed $seed \
     -b 150 $gpu \
+    $s \
     --num_mlp_layers ${num_mlp_layers} \
     --input_model ${model_dir}/model_${model_num}.pth"
 
