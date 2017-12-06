61c6be02a77569f1f019e399c91f27d7ab5e7bb7
roys174 <roysch@cs.washington.edu>
2017-12-06 10:48:28 -0800

    Added a few features to gen_test_results

--- scripts/get_test_result.sh
+++ scripts/get_test_result.sh
@@ -2,11 +2,17 @@
 
 model=0
 
+# dev loss index
+ind=16
 if [ $# -lt 1 ]; then
-	echo "Usage: $0 <dataset> <model=$model>"
+	echo "Usage: $0 <dataset> <model=$model> <maximize accuracy rather than loss>"
 	exit -1
 elif [ $# -gt 1 ]; then
 	model=$2
+	if [ $# -gt 2 ]; then
+		# dev acc index
+		ind='18 -r'
+	fi
 fi
 
 dataset=$1
@@ -16,18 +22,24 @@ wdir=$HOME/work/soft_patterns/
 
 if [ $model -eq 1 ]; then
 	wdir=$wdir/baselines/dan_b150/$dataset/
-	s=''
+	s='.out'
 elif [ $model -eq 2 ]; then
 	wdir=$wdir/baselines/lstm/$dataset/
 	s=''
 else
-	s="_${1}_seed"
+	s="_${1}_seed*/output.dat"
 fi
 
+l=$(ls $wdir/*${s} | wc -l | awk '{print $1}')
+echo "$l files found.i Latest:"
+
+ls -ltr $wdir/*${s} | tail -n1 | cut -d' ' -f6-9
+
 echo "Best dev:"
-grep loss: $wdir/*${s}*/output.dat | sort -rnk 18 | head -n1 | awk '{print $18}' 
-f=$(grep loss: $wdir/*${s}*/output.dat | sort -nk 16 | head -n 1 | cut -d: -f1)
-n=$(grep loss: $wdir/*${s}d*/output.dat | sort -nk 16 | head -n 1 | awk '{print $2}')
+
+grep loss: $wdir/*${s} | sort -rnk 18 | head -n1 | awk '{print $18}' 
+f=$(grep loss: $wdir/*${s} | sort -nk $ind | head -n 1 | cut -d: -f1)
+n=$(grep loss: $wdir/*${s} | sort -nk $ind | head -n 1 | awk '{print $2}')
 
 echo "Test: $f, $n"
 
