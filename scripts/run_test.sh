8f279fc1a2cfd6a210606e8b2729b2779e979d47
Roy Schwartz <roysch@pinot.cs.washington.edu>
2018-01-16 14:25:53 -0800

    run_test.sh now has a gpu flag

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,8 +4,8 @@ set -e
 
 model_num=-1
 
-if [ "$#" -lt 5 ]; then
-    echo "Usage: $0 <output file> <model: 0 for soft_patterns, 1 for dan, 2 for lstm> <model number> <test data> <test labels>"
+if [ "$#" -lt 6 ]; then
+    echo "Usage: $0 <output file> <model: 0 for soft_patterns, 1 for dan, 2 for lstm> <model number (or -1 for best)> <test data> <test labels> <gpu num (or -1)>"
     exit -1
 fi
 
@@ -14,6 +14,7 @@ model=$2
 model_num=$3
 test_data=$4
 test_labels=$5
+gpu=$6
 
 n=$(echo $f | awk -F / '{print $NF}')
 
@@ -48,13 +49,19 @@ if [ ${model_num} -eq -1 ]; then
 	exit -2
 fi
 
+if [ $gpu -eq -1 ]; then
+    gpu_flag=''
+else
+    gpu_flag='--gpu'
+fi
+
 
 e=$(get_param ${f} embedding_file)
-gpu=$(get_param ${f} gpu)
 seed=$(get_param ${f} seed)
 mlp_hidden_dim=$(get_param ${f} mlp_hidden_dim)
 num_mlp_layers=$(get_param ${f} num_mlp_layers)
 hidden_dim=$(get_param ${f} hidden_dim)
+
 
 if [ $model -eq 0 ]; then
     maxplus=$(get_param ${f} maxplus)
@@ -62,8 +69,10 @@ if [ $model -eq 0 ]; then
 
     patterns=$(get_param ${f} patterns)
     rnn=$(get_param ${f} use_rnn)
+    no_sl=$(get_param ${f} no_sl)
+    no_eps=$(get_param ${f} no_eps)
 
-    s="$patterns $maxplus $maxtimes"
+    s="$patterns $maxplus $maxtimes $no_sl $no_eps"
 elif [ $model -eq 1 ]; then
     s="--dan"
 elif [ $model -eq 2 ]; then
@@ -82,7 +91,7 @@ com="python -u soft_patterns_test.py  \
     --tl '' \
     ${mlp_hidden_dim} \
     $seed \
-    -b 150 $gpu \
+    -b 150 $gpu_flag \
     $s \
     $rnn \
     $hidden_dim \
@@ -120,7 +129,9 @@ if [[ "$HOSTNAME" == *.stampede2.tacc.utexas.edu ]]; then
     f=$(gen_cluster_file ${n})
 
     sbatch ${f}
-else
+elif [ $gpu -eq -1 ]; then
     ${com} | tee ${model_dir}/test_results.dat
+else
+    export CUDA_VISIBLE_DEVICES=$gpu &&  ${com} | tee ${model_dir}/test_results.dat
 fi
 
