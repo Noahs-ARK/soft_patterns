6319bc1b98cb49697ef85e0b102cd4b7c47aae92
willandersson1 <andersson.1@bluewin.ch>
2022-06-10 23:23:20 +0200

    Okay now it does something woohoo

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 # Use this instead of having to copy/paste into shell each time
-EMB_FILE='<word_embeddings_file>'
+EMB_FILE='test/data/glove.6B.50d.20words.txt'
 TR_DATA='data/train.data'
 TR_LABELS='data/train.labels'
 DEV_DATA='data/dev.data'
 DEV_LABELS='data/dev.labels'
-PATTERN='<pattern specification>'
-MODEL_SAVE_DIR='<output model directory>'
+PATTERN='5-50_4-50_3-50_2-50' # "Pattern lengths and numbers: an underscore separated list of length-number pairs (default: 5-50_4-50_3-50_2-50)". Taken from one of the files
+MODEL_SAVE_DIR='models_blabla/'
 
 python3.6 ./soft_patterns.py -e "$EMB_FILE" --td "$TR_DATA" --tl "$TR_LABELS" --vd "$DEV_DATA" --vl "$DEV_LABELS" -p "$PATTERN" --model_save_dir "$MODEL_SAVE_DIR"
