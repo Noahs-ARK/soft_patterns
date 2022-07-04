82c73a84c6b4571c926bdbd65b8740864b9ed64d
Antares01 <meziumichele@gmail.com>
2022-07-04 13:52:59 +0200

    mezium

--- train.sh
+++ train.sh
@@ -1,10 +1,14 @@
 # Use this instead of having to copy/paste into shell each time
-EMB_FILE='test/data/glove.6B.50d.20words.txt'
+EMB_FILE='test/data/glove.840B.300d.txt'
 TR_DATA='data/train.data'
 TR_LABELS='data/train.labels'
 DEV_DATA='data/dev.data'
 DEV_LABELS='data/dev.labels'
-PATTERN='5-50_4-50_3-50_2-50' # "Pattern lengths and numbers: an underscore separated list of length-number pairs (default: 5-50_4-50_3-50_2-50)". Taken from one of the files
+PATTERN='8-10_7-10_6-10_5-10_4-10_3-10_2-10'  #'5-50_4-50_3-50_2-50' # "Pattern lengths and numbers: an underscore separated list of length-number pairs (default: 5-50_4-50_3-50_2-50)". Taken from one of the files
 MODEL_SAVE_DIR='models_blabla/'
+LEARNING_RATE=0.001
+MLP_DIM=100
+DROPOUT=0.05
+SEMIRING='LogSpaceMaxTimesSemiring'
 
-python3.6 ./soft_patterns.py -e "$EMB_FILE" --td "$TR_DATA" --tl "$TR_LABELS" --vd "$DEV_DATA" --vl "$DEV_LABELS" -p "$PATTERN" --model_save_dir "$MODEL_SAVE_DIR"
+python3.6 ./soft_patterns.py -i "70" -l "$LEARNING_RATE" -d $MLP_DIM -t "$DROPOUT" --semiring "$SEMIRING" -e "$EMB_FILE" --td "$TR_DATA" --tl "$TR_LABELS" --vd "$DEV_DATA" --vl "$DEV_LABELS" -p "$PATTERN" --model_save_dir "$MODEL_SAVE_DIR" --no_eps
